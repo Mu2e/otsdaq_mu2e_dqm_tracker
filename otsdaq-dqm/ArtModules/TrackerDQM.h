d7dd64ad57433b6107b5dd4598e7274e637d4abe
Greg <grakness@FNAL.GOV>
2020-11-03 15:39:56 -0600

    Checking in otsdaq_mu2e_dqm in trigger working area

--- otsdaq-dqm/ArtModules/TrackerDQM.h
+++ otsdaq-dqm/ArtModules/TrackerDQM.h
@@ -5,7 +5,7 @@
 #include "art_root_io/TFileService.h"
 #include "DataProducts/inc/TrkTypes.hh"
 #include "DataProducts/inc/StrawId.hh"
-
+#include "otsdaq/Macros/ProcessorPluginMacros.h"
 
 namespace ots {
 
@@ -29,28 +29,37 @@ namespace ots {
 	}
 
 	void straw_fill(HistoContainer *histos, int data, std::string title, art::ServiceHandle<art::TFileService> tfs, mu2e::StrawId sid) {
+		__MOUT__ << title.c_str() + std::to_string(sid.station()) + " " + std::to_string(sid.plane()) + " " + std::to_string(sid.panel()) + " " + std::to_string(sid.straw()) << std::endl;
+
 		if(histos->histograms.size() == 0){
-                                        histos->BookHistos(tfs, title.c_str(), sid);
-                                        histos->histograms[0]._Hist->Fill(data);
+                                        __MOUT__ << "No histograms booked. Should they have been created elsewhere?" << std::endl;
+
+					//histos->BookHistos(tfs, title.c_str() + std::to_string(sid.station()) + " " + std::to_string(sid.plane()) + " " + std::to_string(sid.panel()) + " " + std::to_string(sid.straw()), sid);
+                                        //histos->histograms[0]._Hist->Fill(data);
                                 } else {
                                         for(int histIdx = 0; histIdx < int(histos->histograms.size()); ++histIdx){
-                                                if((sid.straw() == histos->histograms[histIdx].straw) && (sid.panel() == histos->histograms[histIdx].panel) && (sid.plane() == histos->histograms[histIdx].plane) && (sid.station() == histos->histograms[histIdx].station)) { // if the histogram does exist, fill it
+						if((sid.straw() == histos->histograms[histIdx].straw) && (sid.panel() == histos->histograms[histIdx].panel) && (sid.plane() == histos->histograms[histIdx].plane) && (sid.station() == histos->histograms[histIdx].station)) { // if the histogram does exist, fill it
                                                         histos->histograms[histIdx]._Hist->Fill(data);
+							__MOUT__ << "number of histos: " << histos->histograms.size() << std::endl;
                                                         break;
                                                 }
-                                                if(histIdx == int(histos->histograms.size() - 1)) { //if the histogram doesn't exist, book it
-                                                        histos->BookHistos(tfs, title.c_str() + std::to_string(sid.station()) + " " + std::to_string(sid.plane()) + " " + std::to_string(sid.panel()) + " " + std::to_string(sid.straw()), sid);
-                                                        histos->histograms[histIdx]._Hist->Fill(data);
+
+						if(histIdx == int(histos->histograms.size() - 1)) { //if the histogram doesn't exist, book it
+                                                        //histos->BookHistos(tfs, title.c_str() + std::to_string(sid.station()) + " " + std::to_string(sid.plane()) + " " + std::to_string(sid.panel()) + " " + std::to_string(sid.straw()), sid);
+                                                        //histos->histograms[histIdx]._Hist->Fill(data);
+							__MOUT__ << "Cannot find histogram: " << title + std::to_string(sid.station()) + " " + std::to_string(sid.plane()) + " " + std::to_string(sid.panel()) + " " + std::to_string(sid.straw()) << std::endl;
                                                 }
+
                                         }
                                 }
-
 	}
 
 	void panel_fill(HistoContainer *histos, int data, std::string title, art::ServiceHandle<art::TFileService> tfs, mu2e::StrawId sid) {
 	if(histos->histograms.size() == 0){
-        	histos->BookHistos(tfs, title.c_str(), sid);
-        	histos->histograms[0]._Hist->Fill(data);
+        	 __MOUT__ << "No histograms booked. Should they have been created elsewhere?" << std::endl;
+
+		//histos->BookHistos(tfs, title.c_str(), sid);
+        	//histos->histograms[0]._Hist->Fill(data);
         } else {
         	for(int histIdx = 0; histIdx < int(histos->histograms.size()); ++histIdx){
                 	if((sid.panel() == histos->histograms[histIdx].panel) && (sid.plane() == histos->histograms[histIdx].plane) && (sid.station() == histos->histograms[histIdx].station)) {
